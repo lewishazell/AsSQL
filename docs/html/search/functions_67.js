a1af9182b17038aebca65ca5da65a90a16268139
Lewis-H <sdvb.git@mailnull.com>
2015-04-06 11:01:50 +0100

    Updated documentation.

--- docs/html/search/functions_67.js
+++ docs/html/search/functions_67.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['getconnector',['getConnector',['../classAsSQL.html#a36e47e4a2da43c26271fcd4e778aceb5',1,'AsSQL\getConnector()'],['../classPollState.html#a63855e4ec33c70bb5b2f0bafdf7a7daf',1,'PollState\getConnector()']]],
+  ['getcount',['getCount',['../classPollPool.html#ae4e298252e9945212c70a2453dff2a5a',1,'PollPool']]],
   ['gettag',['getTag',['../classAsyncResult.html#a00fc9c765d0c13fece6f70e512c697a4',1,'AsyncResult']]],
   ['getwrapper',['getWrapper',['../classPollState.html#ab83dc50a315fdd79ddddfb82b68a39c5',1,'PollState']]]
 ];
